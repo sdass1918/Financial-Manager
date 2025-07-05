88e46a8ec67d2ef1e905d045798185e4536251c9
Sudipta <sd1216826@gmail.com>
2025-07-05 13:14:45 +0530

    Final

--- app/api/transactions/[id]/route.ts
+++ app/api/transactions/[id]/route.ts
@@ -4,10 +4,13 @@ import { connectDB } from '@/lib/mongo';
 import { Transaction } from '@/models/Transaction';
 import { NextResponse } from 'next/server';
 
-export async function DELETE(req: Request, context: { params: { id: string } }) {
+export async function DELETE(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
   await connectDB();
 
-  const { id } = context.params;
+  const { id } = await params;
   if (!id) {
     return NextResponse.json({ error: 'Missing ID' }, { status: 400 });
   }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript").config({
+  ...compat.config({
   extends: ['next'],
     rules: {
       'react/no-unescaped-entities': 'off',
